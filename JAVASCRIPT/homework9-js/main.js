3b02300abe4477aba6b7c17f4fc90d5ba31c90f4
andriivna <farinaanna686@gmail.com>
2022-08-21 13:18:51 +0300

    add index.html file

---
+++ JAVASCRIPT/homework9-js/main.js
@@ -0,0 +1,366 @@
+// - є масив
+// let simpsons = [
+//     {
+//         name: 'Bart',
+//         surname: 'Simpson',
+//         age: 10,
+//         info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+//     },
+//     {
+//         name: 'Homer',
+//         surname: 'Simpson',
+//         age: 40,
+//         info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+//         photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+//     },
+//     {
+//         name: 'Marge',
+//         surname: 'Simpson',
+//         age: 38,
+//         info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+//     },
+//     {
+//         name: 'Lisa',
+//         surname: 'Simpson',
+//         age: 9,
+//         info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+//     },
+//     {
+//         name: 'Maggie',
+//         surname: 'Simpson',
+//         age: 1,
+//         info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+//     },
+// ];
+//
+//
+// Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
+//
+//     Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
+//
+// let simpsons = [
+//     {
+//         name: 'Bart',
+//         surname: 'Simpson',
+//         age: 10,
+//         info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+//     },
+//     {
+//         name: 'Homer',
+//         surname: 'Simpson',
+//         age: 40,
+//         info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+//         photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+//     },
+//     {
+//         name: 'Marge',
+//         surname: 'Simpson',
+//         age: 38,
+//         info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+//     },
+//     {
+//         name: 'Lisa',
+//         surname: 'Simpson',
+//         age: 9,
+//         info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+//     },
+//     {
+//         name: 'Maggie',
+//         surname: 'Simpson',
+//         age: 1,
+//         info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+//     }
+//     ]
+//
+//
+// let targets = document.getElementsByClassName('member');
+// let target = targets[0];
+//
+// for (const member of simpsons) {
+//     let div = document.createElement('div');
+//     div.classList.add('member-box')
+//
+//     let memberName = document.createElement('h2');
+//     memberName.innerText = `${member.name} ${member.surname} ${member.age}`;
+//     div.appendChild(memberName);
+//
+//     let inf = document.createElement('p');
+//     inf.innerText = `${member.info}`;
+//     div.appendChild(inf);
+//
+//     let img = document.createElement('img');
+//     img.src = member.photo;
+//     div.appendChild(img);
+//
+//     target.appendChild(div);
+//
+// }
+
+
+// Цикл в циклі
+// - Є масив
+// let coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
+//
+// let targets = document.getElementsByClassName('target');
+// let target = targets[0];
+//
+// for (const course of coursesArray) {
+//     let div = document.createElement('div');
+//     div.classList.add('info-box');
+//
+//     let courseName = document.createElement('h2');
+//     courseName.innerText = `${course.title}`;
+//     div.appendChild(courseName);
+//
+//     let courseduration = document.createElement('h4');
+//     courseduration.innerText = `Duration is ${course.monthDuration} months`;
+//     div.appendChild(courseduration);
+//
+//     let hourly = document.createElement('h4');
+//     hourly.innerText = `Hourly duration is ${course.hourDuration} hours`;
+//     div.appendChild(hourly);
+//
+//
+//     let listOfModules = document.createElement('ul');
+//
+//         for (const list of course.modules) {
+//
+//             let li = document.createElement('li');
+//             li.innerText = list;
+//            listOfModules.appendChild(li);
+//             div.appendChild(listOfModules);
+//
+//
+//         }
+//
+//     target.appendChild(div);
+//
+// }
+
+
+
+// //     - створити блок,
+// let div = document.createElement('div');
+//
+// //     - додати йому класи wrap, collapse, alpha, beta
+// div.classList.add('wrap');
+// div.classList.add('collapse');
+// div.classList.add('alpha');
+// div.classList.add('beta');
+// // - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// div.style.background = 'yellow';
+// div.style.color = 'pink';
+// div.style.fontSize = '16px';
+//
+// // - додати цей блок в body.
+// div.body.appendChild(div);
+// // - клонувати його повністю, та додати клон в body.
+// document.body.appendChild(div.cloneNode(true));
+//
+
+
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+//
+//
+// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// Завдання робити через цикли.
+//
+// let list2 = document.getElementsByClassName('courses')[0];
+// for (const course of coursesAndDurationArray) {
+//
+//     let div = document.createElement('div');
+//     div.classList.add('info-box');
+//
+//     let courseName = document.createElement('h3');
+//     courseName.innerText = `${course.title} - ${course.monthDuration}`;
+//     div.appendChild(courseName);
+//
+//     list2.appendChild(div);
+//
+// }
+
+
+// // - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// //
+// //
+// // За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+// //     Завдання робити через цикли.
+// //
+// for (const course of coursesAndDurationArray) {
+//
+//     let target = document.getElementsByClassName('items')[0];
+//
+//     let div = document.createElement('div');
+//     div.classList.add('heading', 'p');
+//
+//
+//
+//     let name = document.createElement('h2');
+//     name.innerText = `${course.title}`;
+//     div.appendChild(name);
+//
+//
+//     let text = document.createElement('p');
+//     text.innerText = `Duration of this course is ${course.monthDuration}`;
+//     div.appendChild(text);
+//
+//
+//     target.appendChild(div);
+//
+// }
+
+
+
+//     - Створити довільний елемент з id = text.  Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
+//
+//     document.getElementById('remover').onclick = function() {
+//     document.getElementById('text').hidden = true;
+// }
+
+//
+//     - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+
+// console.log(document.forms.form1.login);
+//
+
+// let form = document.createElement('form');
+// form.name = 'f1';
+// document.body.appendChild(form);
+//
+// let input = document.createElement('input');
+// form.appendChild(input);
+//
+// let button = document.createElement('button');
+// button.innerText = 'send';
+// form.appendChild(button);
+//
+// let form1 = document.forms.f1;
+//
+// form1.onsubmit = function (e) {
+//     e.preventDefault()
+//     if (input.value < 18) {
+//         alert("You're too young.");
+//     }else if (input.value >= 18) {
+//         alert('Your age is OK)');
+//     } else {
+//         alert('Enter your age');
+//     }
+// };
+
+// let form=document.createElement('form');
+// form.id='form';
+// document.body.appendChild(form);
+//
+// let input=document.createElement('input');
+// input.type='number';
+// input.name='age';
+// input.placeholder = 'Enter your age';
+//
+// let button=document.createElement('button');
+// button.innerText='send';
+//
+// form.append(input, button);
+//
+// form.addEventListener('submit',e=>{
+//     e.preventDefault();
+//     if(+e.target.age.value< 18){
+//         alert("You're too young");
+//     }else {
+//         alert('Your age is OK');
+//     }
+// })
+
+
+
